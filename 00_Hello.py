1a1f14afdab08c7a65c632c91768c114830fee71
Miguel Gutierrez <miguelgutitami@gmail.com>
2022-10-19 13:11:57 +0200

    Primer guardado en git

---
+++ 00_Hello.py
@@ -0,0 +1,24 @@
+# Esto es un comentario:
+# Nuestro Hola mundo en Python
+print("Hola Python")
+print('Hola Python')
+print(type ('Hola Python'))
+
+"""
+Esto es 
+un comentario
+en varias lineas
+"""
+
+'''
+Este también
+es un comentario
+en varias lineas
+'''
+
+#Consultar el tipo de dato
+print(type ('soy un dato str')) #Tipo 'str'
+print(type (5)) #Tipo 'int'
+print(type (1.5)) #Tipo 'float'
+print(type (True)) #Tipo 'bool'
+print(type (3 + 1j)) #Tipo 'str'
